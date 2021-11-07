1e82a73f53cd61b222f2180d8a32b362559a3de0
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-07 21:52:41 +0100

    Contact State Provider Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-function App() {
+import Navbar from "./component/layouts/Navbar";
+import About from "./component/pages/About";
+import Home from "./component/pages/Home";
+import ContactState from "./context/contact/contactState";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
-}
+};
 
 export default App;
