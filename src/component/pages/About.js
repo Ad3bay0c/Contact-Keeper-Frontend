142787a39c97b857af8d717c49017d89e35634e9
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-07 22:26:23 +0100

    About the Application

---
+++ src/component/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className="my-1">
+        This is Contact Saver Application developed with Golang(backend) and
+        React (frontend)
+      </p>
+      <p className="bg-dark p">
+      <strong>Developed By Clinton (G-Tech) </strong>    <strong>Version: 1.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
