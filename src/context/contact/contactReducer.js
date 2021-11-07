d1a4b5df5ff47699772f54b735fd063290d447d2
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-07 22:27:48 +0100

    Contact Reducer Created

---
+++ src/context/contact/contactReducer.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  ADD_CONTACT,
+  CLEAR_CURRENT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  UPDATE_CONTACT,
+} from "../type";
+
+const contactReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) =>
+          contact.id === action.payload.id ? action.payload : contact
+        ),
+        current: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default contactReducer;
