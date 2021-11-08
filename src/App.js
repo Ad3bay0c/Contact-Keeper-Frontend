069ccd53d719d0ab3915eb32b59c04a1d9637162
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-08 21:37:04 +0100

    Auth State tag added to the app.js

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import Navbar from "./component/layouts/Navbar";
 import About from "./component/pages/About";
 import Home from "./component/pages/Home";
 import ContactState from "./context/contact/contactState";
+import AuthState from "./context/auth/authState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
