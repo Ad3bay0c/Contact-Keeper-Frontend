27d968045e47b8d43afb20ee5ec9afa876b026d9
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-09 13:47:53 +0100

    Load User and Register actions added

--- src/context/auth/authState.js
+++ src/context/auth/authState.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import axios from "axios";
 import AuthReducer from "./authReducer";
 import AuthContext from "./authContext";
 import {
@@ -9,8 +10,9 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGOUT,
-  CLEAR_ERRORS
+  CLEAR_ERRORS,
 } from "../type";
+import SetAuthToken from "../../utils/SetAuthToken";
 
 const AuthState = (props) => {
   const initialState = {
@@ -18,19 +20,74 @@ const AuthState = (props) => {
     user: null,
     token: localStorage.getItem("token"),
     loading: true,
-    error: null
+    error: null,
   };
 
   const [state, dispatch] = useReducer(AuthReducer, initialState);
 
+  //Register user
+  const RegisterUser = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post(
+        "http://localhost:2500/api/auth",
+        formData,
+        config
+      );
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+      LoadUser();
+    } catch (err) {
+      console.log(err.response.data);
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.error,
+      });
+    }
+  };
+
+  //Clear Error
+  const clearErrors = () => {
+    dispatch({
+      type: CLEAR_ERRORS,
+    });
+  };
+
+  // Load User
+  const LoadUser = async () => {
+    if (localStorage.token) {
+      SetAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.get("http://localhost:2500/api/user");
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data.user,
+      });
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR,
+      });
+    }
+  };
   return (
-    <AuthContext.Provider value={{
-      isAuthenticated: state.isAuthenticated,
-      user: state.user,
-      token: state.token,
-      loading: state.loading,
-      error: state.error,
-    }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated: state.isAuthenticated,
+        user: state.user,
+        token: state.token,
+        loading: state.loading,
+        error: state.error,
+        RegisterUser,
+        clearErrors,
+      }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
