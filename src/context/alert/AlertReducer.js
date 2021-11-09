3092ee6f535af75f8ed19aad643d72050835a3ee
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-09 13:45:37 +0100

    Alert reducer created and logics added

--- src/context/alert/AlertReducer.js
+++ src/context/alert/AlertReducer.js
@@ -5,7 +5,7 @@ const AlertReducer = (state, action) => {
     case SET_ALERT:
       return [...state, action.payload];
     case REMOVE_ALERT:
-      return state.filter((alert) => alert.id != action.payload);
+      return state.filter((alert) => alert.id !== action.payload);
     default:
       return state;
   }
