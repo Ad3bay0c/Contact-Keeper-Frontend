31e6cd237d515177851ab469d7c8dfd84ab02a85
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-10 09:38:49 +0100

    Add Contact freducer logic implemented

--- src/context/contact/contactReducer.js
+++ src/context/contact/contactReducer.js
@@ -2,18 +2,27 @@ import {
   ADD_CONTACT,
   CLEAR_CURRENT,
   CLEAR_FILTER,
+  CONTACT_ERROR,
   DELETE_CONTACT,
   FILTER_CONTACTS,
+  GET_CONTACT,
   SET_CURRENT,
   UPDATE_CONTACT,
 } from "../type";
 
 const contactReducer = (state, action) => {
   switch (action.type) {
+    case GET_CONTACT:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload],
+        contacts: [action.payload, ...state.contacts],
+        loading: false,
       };
     case DELETE_CONTACT:
       return {
@@ -21,6 +30,7 @@ const contactReducer = (state, action) => {
         contacts: state.contacts.filter(
           (contact) => contact.id !== action.payload
         ),
+        loading: false,
       };
     case SET_CURRENT:
       return {
@@ -36,9 +46,10 @@ const contactReducer = (state, action) => {
       return {
         ...state,
         contacts: state.contacts.map((contact) =>
-          contact.id === action.payload.id ? action.payload : contact
+          contact._id === action.payload.id ? action.payload : contact
         ),
         current: null,
+        loading: false,
       };
     case FILTER_CONTACTS:
       return {
@@ -52,6 +63,11 @@ const contactReducer = (state, action) => {
       return {
         ...state,
         filtered: null,
+      };
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload,
       };
     default:
       return state;
