e426c9325ecc4470784b22f575f25b8dcd5e1864
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-10 08:57:14 +0100

    LoadUser function added to useEffect hook

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
 import Contact from "../contacts/Contact";
 import ContactFilter from "../contacts/ContactFilter";
 import ContactForm from "../contacts/ContactForm";
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  const { LoadUser } = authContext;
+  useEffect(() => {
+    LoadUser();
+    //eslint-disable-next-line
+  }, []);
   return (
     <div className="grid-2">
       <div>
         <ContactForm />
       </div>
       <div>
-        <ContactFilter /> 
+        <ContactFilter />
         <Contact />
-      </div>  
+      </div>
     </div>
   );
 };
